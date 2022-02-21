8f55b94f05f9a3c1b65bbea93f6b4d59540c84de
Vladimir <vlastroganov@gmail.com>
2022-02-22 00:19:58 +0300

    Basic of the web site.

---
+++ Models/Joke.cs
@@ -0,0 +1,13 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+        public Joke()
+        {
+
+        }
+    }
+}
